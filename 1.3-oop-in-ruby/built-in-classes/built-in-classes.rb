b47577df16a31f669141dd8eb0c9ac511f1d7689
John McIntosh II <jmci10@hotmail.com>
2016-01-07 03:16:26 +0000

    finish built in classes

--- 1.3-oop-in-ruby/built-in-classes/built-in-classes.rb
+++ 1.3-oop-in-ruby/built-in-classes/built-in-classes.rb
@@ -8,7 +8,7 @@ the_string = "asfd jl;j;"
 number = 100
 
 # variables hold references to objects
-x = "asd"
+a = "asd"
 
 # variable assignment causes object references to be shared
 y = "asd"
@@ -17,3 +17,22 @@ puts a # "asd"
 
 puts a.object_id # 18954980
 puts y.object_id # 18954980
+
+# if object is mutable, a change in one variable will be reflected in the other
+puts a.gsub!(/a/, "z") # a is now "zsd"
+puts y # y is now "zsd"
+
+# reassignment has no effect on the other
+a = "asd"
+puts y # y is still "zsd"
+
+# mutable object can be made immutable with freeze method
+a.freeze # frozen string
+# a.gsub!(/s/, "t") # `gsub!': can't modify frozen String (RuntimeError)
+
+# symbols act kind of like immutable strings, multiple uses of a symbol all reference the same value
+colors = [:red, :green, :blue, :orange, :yellow]
+puts initial_color = colors[0].to_s # "red"
+
+# arrays of symbols shortcut using %i
+vegetables = %i[carrots broccoli peas spinach] # [:carrots, :broccoli, :peas, :spinach]

81634f0479ba6e86669c05ffb522e02c4b860150
John McIntosh II <jmci10@hotmail.com>
2016-01-16 07:59:57 +0000

    methods and attributes continue again

--- 1.3-oop-in-ruby/methods-and-attributes/methods-attributes.rb
+++ 1.3-oop-in-ruby/methods-and-attributes/methods-attributes.rb
@@ -16,3 +16,26 @@ File.open(filename) { |f| f.some_action }
 
 # methods can take a variable number of arguments
 receiver.method(arg1, *more_args)
+
+# an asterisk in a list of formal parameters can "collapse" a sequence of actual parameters into an array
+def mymethod(a, b, *c)
+  print a, b
+  c.each do |x| 
+    print x 
+  end
+end
+
+mymethod(1, 2, 3, 4, 5, 6, 7) # a = 1, b = 2, c = [3, 4, 5, 6, 7]
+
+# named parameters simultaneously set default values and allow arguments to be given in any order because they are explicitly labeled
+def name_parameter_method(name: "default", options: {})
+  options.merge!(name: name)
+  some_action_with(options)
+end
+
+# named parameter with default omitted in method definition, named parameter is required
+def other_method(name:, age:)
+  puts "Person #{name} is aged #{age}."
+  # It's an error to call this method without specifying
+  # values for name and age.
+end

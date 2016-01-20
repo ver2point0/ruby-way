58131ec39b06f0f0b842377f68a8889a74561236
John McIntosh II <jmci10@hotmail.com>
2016-01-20 06:06:47 +0000

    code at runtime continued

--- 1.4-dynamic-aspects-ruby/coding-runtime/code-runtime.rb
+++ 1.4-dynamic-aspects-ruby/coding-runtime/code-runtime.rb
@@ -11,3 +11,15 @@ end
 puts calculate(2, "+", 4) # 6
 puts calculate(5, "*", "@alpha") # 125
 puts calculate("@beta", "**", 3) # 1728
+
+puts "Method name: "
+meth_name = gets
+puts "Line of code: "
+code = gets
+
+# build the string
+string = %[def #{meth_name}\n #{code}\n end]
+# define method
+eval(string)
+# call method
+eval(meth_name)

5c7b61a66f006f4c9dcc608a8954f3e83131f9f9
John McIntosh II <jmci10@hotmail.com>
2016-01-24 07:24:25 +0000

    finish code at runtime

--- 1.4-dynamic-aspects-ruby/coding-runtime/code-runtime.rb
+++ 1.4-dynamic-aspects-ruby/coding-runtime/code-runtime.rb
@@ -23,3 +23,28 @@ string = %[def #{meth_name}\n #{code}\n end]
 eval(string)
 # call method
 eval(meth_name)
+
+# evaluate flags at runtime to help maintain a single code base (flag may be tested many times in the course of execution)
+if platform == Windows
+  # action1
+elsif platform == Linux
+  # action2
+else 
+  # default_action
+end
+
+# reduce small runtime penalty with this code by enclosing platform-dependent code in a method used across all platforms
+if platform == Windows
+  def my_action
+    # action1
+  end
+elsif platform == Linux
+  def my_action
+    # action2
+  end
+else
+  def my_action
+    # default_action
+  end
+end
+    

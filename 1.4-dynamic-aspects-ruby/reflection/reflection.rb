ff15606c1150615c260c12d4389f1825bff03384
John McIntosh II <jmci10@hotmail.com>
2016-01-27 03:45:25 +0000

    finish reflection

--- 1.4-dynamic-aspects-ruby/reflection/reflection.rb
+++ 1.4-dynamic-aspects-ruby/reflection/reflection.rb
@@ -7,3 +7,31 @@ else
 end
 
 # respond_to? method determines whether an object can respond to the specified method call (is that method defined for that object)
+
+# created Dog and Animal classes so is_a? and kind_of? methods work on the command line
+class Dog
+  def initialize
+  end
+end
+
+class Animal < Dog
+end
+
+# is_a? and kind_of? methods
+puts "abc".class # String
+puts 345.class # Fixnum
+rover = Dog.new
+
+puts rover.class # Dog
+
+if rover.is_a? Dog
+  puts "Of course he is a dog."
+end
+
+if rover.kind_of? Dog
+  puts "Yes, he is still a dog."
+end
+
+if rover.is_a? Animal
+  puts "Yes, he's an animal, too."
+end

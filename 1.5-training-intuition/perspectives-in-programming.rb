ba10a4ce20df9801503b51210189f47076567adc
John McIntosh II <jmci10@hotmail.com>
2016-02-24 10:09:09 +0000

    start pip

---
+++ 1.5-training-intuition/perspectives-in-programming.rb
@@ -0,0 +1,12 @@
+# There is not boolean type in Ruby. TrueClass and FalseClass are distinct classes
+# Their only instantiations are true and false
+
+# Many Ruby operators are similar or identical to C
+# Two notable ones are ++ and --, they are not available in Ruby
+# neither in "pre" or "post" forms
+
+# Ruby's modulus behavior
+# puts (5 % 3) # prints 2
+# puts (-5 % 3) # prints 1
+# puts (5 % -3) # prints -1
+# puts (-5 % -3) # prints -2

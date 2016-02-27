7876987fc5f88ac677029d2246bc4894edc4f1da
John McIntosh II <jmci10@hotmail.com>
2016-02-27 02:47:07 +0000

    pip again

--- 1.5-training-intuition/perspectives-in-programming.rb
+++ 1.5-training-intuition/perspectives-in-programming.rb
@@ -15,3 +15,12 @@
 # In Ruby, everything is true except false and nil
 
 # In Ruby, variables don't classes; only values have classes
+
+# No declarations of variables in Ruby.
+# good practice to assign nil to variable initially, informs parser that it is variable name rather than a method name
+
+# ARGV[0] is truly the first of the command-line parameters; it is not the file or script name preceding the parameters
+
+# most of ruby's operations are really methods; the "punctuation" form of these methods is provided for familiarity and convenience.
+# first exception is the set of reflexive assignment operators (+=, -=, *=, and so on)
+# second exception is the following set: = .. ... ! not && and || or != !~

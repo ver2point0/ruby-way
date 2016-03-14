34b020322b34a16f23438c909e835e5b21cc1780
John McIntosh II <jmci10@hotmail.com>
2016-03-14 11:39:31 +0000

    start ruby's case statement

---
+++ 1.5-training-intuition/ruby-case-statement.rb
@@ -0,0 +1,15 @@
+# Ruby's case statement has some unique features compared to other languages
+
+# consider the following trivial case statement
+# case expression
+#   when value
+#     some_action
+# end
+
+# the expression is compared with value, some_action is performed if they correspond
+
+# the === (relationship operator) does not always denote equality
+# if value === expression
+#   some_action
+# end
+# this if statement is equivalent to the above case statement

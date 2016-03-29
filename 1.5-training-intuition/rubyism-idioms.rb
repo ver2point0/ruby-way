94294636f61caa3c00d64a166e116b06877bf5f9
John McIntosh II <jmci10@hotmail.com>
2016-03-29 02:15:43 +0000

    start rubyisms and idioms

---
+++ 1.5-training-intuition/rubyism-idioms.rb
@@ -0,0 +1,7 @@
+# alias can be used to give alternate names for global variables and methods
+
+# numbered global variables $1, $2, $3, and so on, cannot be aliased
+
+# "special variables" like $_, $$, and the rest are not recommended
+# if needed, they can be aliased to longer, readable names such as 
+# $LAST_READ_LINE or $PROCESS_ID by using require 'English'

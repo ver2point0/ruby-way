140381106bbf21ce1a0da4daeaa69ff08f12c034
John McIntosh II <jmci10@hotmail.com>
2016-03-30 01:03:42 +0000

    continue rubyisms and idioms

--- 1.5-training-intuition/rubyism-idioms.rb
+++ 1.5-training-intuition/rubyism-idioms.rb
@@ -5,3 +5,7 @@
 # "special variables" like $_, $$, and the rest are not recommended
 # if needed, they can be aliased to longer, readable names such as 
 # $LAST_READ_LINE or $PROCESS_ID by using require 'English'
+
+# the .. and ... range operations are different
+# .. is inclusive of the upper bound and ... is exclusive of the upper bound
+# 5..10 includes 10, 5...10 excludes 10

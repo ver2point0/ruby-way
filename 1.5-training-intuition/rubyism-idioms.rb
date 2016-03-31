89bb80c88450e7d981bb43b3c1443640c2ffa448
John McIntosh II <jmci10@hotmail.com>
2016-03-31 00:02:53 +0000

    continue rubyisms and idioms again

--- 1.5-training-intuition/rubyism-idioms.rb
+++ 1.5-training-intuition/rubyism-idioms.rb
@@ -9,3 +9,12 @@
 # the .. and ... range operations are different
 # .. is inclusive of the upper bound and ... is exclusive of the upper bound
 # 5..10 includes 10, 5...10 excludes 10
+
+# given m..n, the method end will return the endpoint of the range; its alias last with do the same thing
+# these methods will return the the same value (n) for the range m...n even though n is not included in the latter range
+# the method end_excluded? is provided to distinguish between these 2 assignments
+
+# ranges are not arrays, these 2 assignments are different:
+# x = 1..5
+# x = [1, 2, 3, 4, 5]
+# to_a will convert ranges to arrays

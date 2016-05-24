19367a2143e06358ecea83a9e1351145860db338
John McIntosh II <jmci10@hotmail.com>
2016-05-24 05:29:32 +0000

    opening a singleton class

--- 1.6-ruby-jargon-and-slang/ruby-jargon-slang.rb
+++ 1.6-ruby-jargon-and-slang/ruby-jargon-slang.rb
@@ -10,3 +10,13 @@ gamma = 5
 
 delta = alpha(beta(gamma))
 delta = alpha beta gamma # same, but less clear
+
+# opening up a a singleton class
+str = "hello"
+class << str
+  def hyphenated
+    self.split("").join("-")
+  end
+end
+
+str.hyphenated

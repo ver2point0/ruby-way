3c9b7e3bf639440f44e17be76235be790f030998
John McIntosh II <jmci10@hotmail.com>
2016-05-27 10:44:49 +0000

    add double quoted strings

--- 2.1-representing-ordinary-strings/represent-ordinary-strings.rb
+++ 2.1-representing-ordinary-strings/represent-ordinary-strings.rb
@@ -1,3 +1,9 @@
 s1 = 'This is a string'
 s2 = 'Mrs. O\'Leary'
 s3 = 'Look in C:\\TEMP'
+
+s4 = "This is a tab: (\t)"
+s5 = "Some backspaces: xyz\b\b\b"
+s6 = "This is also a tab: \011"
+s7 = "And these are both bells: \a \007"
+s8 = "This is the unicode snowman: \u2603"

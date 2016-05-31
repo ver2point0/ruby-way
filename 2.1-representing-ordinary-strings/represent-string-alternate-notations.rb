fd2c3785f3c9bb867871d391fc940d8744e90745
John McIntosh II <jmci10@hotmail.com>
2016-05-31 10:51:17 +0000

    use different delimiters

--- 2.1-representing-ordinary-strings/represent-string-alternate-notations.rb
+++ 2.1-representing-ordinary-strings/represent-string-alternate-notations.rb
@@ -1,3 +1,11 @@
 s1 = %q[As Magritte said, "Ceci n'est pas une pipe."]
 s2 = %q[This is not a tab: (\t)]
 s3 = %Q[This IS a tab: (\t)]
+
+s4 = %q(Bill said, "Bob said, 'This is a string.'")
+s5 = %q{Another string.}
+s6 = %q<Special characters ''[](){} in this string.>
+
+p s4
+p s5
+p s6

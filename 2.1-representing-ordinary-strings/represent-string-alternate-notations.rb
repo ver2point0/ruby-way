6a73fcc36bc59bbef5f2bd129e1291d400ea3280
John McIntosh II <jmci10@hotmail.com>
2016-06-01 23:17:06 +0000

    non-paired delimiters

--- 2.1-representing-ordinary-strings/represent-string-alternate-notations.rb
+++ 2.1-representing-ordinary-strings/represent-string-alternate-notations.rb
@@ -6,6 +6,5 @@ s4 = %q(Bill said, "Bob said, 'This is a string.'")
 s5 = %q{Another string.}
 s6 = %q<Special characters ''[](){} in this string.>
 
-p s4
-p s5
-p s6
+s7 = %q:"I think Mrs. O'Leary's cow did it,", he said.:
+s8 = %q*\r is a control-M and \n is a control-J.*

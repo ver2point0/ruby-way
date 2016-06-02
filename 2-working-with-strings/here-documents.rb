31e306495dc678dd51e8b70596f7f8e91662c39c
John McIntosh II <jmci10@hotmail.com>
2016-06-02 23:31:11 +0000

    start here documents

---
+++ 2-working-with-strings/here-documents.rb
@@ -0,0 +1,4 @@
+str = <<EOF
+Once upon a midnight dready,
+While I pondered weak and weary,...
+EOF
---
+++ 2-working-with-strings/represent-string-alternate-notations.rb
@@ -0,0 +1,10 @@
+s1 = %q[As Magritte said, "Ceci n'est pas une pipe."]
+s2 = %q[This is not a tab: (\t)]
+s3 = %Q[This IS a tab: (\t)]
+
+s4 = %q(Bill said, "Bob said, 'This is a string.'")
+s5 = %q{Another string.}
+s6 = %q<Special characters ''[](){} in this string.>
+
+s7 = %q:"I think Mrs. O'Leary's cow did it,", he said.:
+s8 = %q*\r is a control-M and \n is a control-J.*

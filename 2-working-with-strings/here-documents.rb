68285deaabf60a2fa188a4f6c734ffbc818f01dd
John McIntosh II <jmci10@hotmail.com>
2016-06-03 20:15:54 +0000

    method call with three strings

--- 2-working-with-strings/here-documents.rb
+++ 2-working-with-strings/here-documents.rb
@@ -2,3 +2,13 @@ str = <<EOF
 Once upon a midnight dready,
 While I pondered weak and weary,...
 EOF
+
+some_method(<<STR1, <<STR2, <<STR3)
+first piece
+of text...
+STR1
+second piece...
+STR2
+third piece
+of text.
+STR3

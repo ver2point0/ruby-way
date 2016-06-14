6f67730d776993e9246c4c4e6c287c6b43f4aa22
John McIntosh II <jmci10@hotmail.com>
2016-06-14 12:51:23 +0000

    method chaining with iterators

--- 2-working-with-strings/process-one-line-at-once.rb
+++ 2-working-with-strings/process-one-line-at-once.rb
@@ -1,6 +1,11 @@
-str = "Once upon\aa time...\nThe End\n"
-num = 0
-str.each_line do |line|
-  num += 1
-  print "Line #{num}: #{line}"
+# str = "Once upon\aa time...\nThe End\n"
+# num = 0
+# str.each_line do |line|
+#   num += 1
+#   print "Line #{num}: #{line}"
+# end
+
+str = "Once upon\na time...\nThe End\n"
+str.each_line.with_index do |line, num|
+  print "Line #{num + 1}: #{line}"
 end

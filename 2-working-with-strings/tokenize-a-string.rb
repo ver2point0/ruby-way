e88e60923cae946674c2f0415ae063055484eb48
John McIntosh II <jmci10@hotmail.com>
2016-06-22 13:13:59 +0000

    start tokenizing strings

---
+++ 2-working-with-strings/tokenize-a-string.rb
@@ -0,0 +1,11 @@
+s1 = "It was a dark and stormy night."
+words = s1.split
+# ["It", "was", "a", "dark", "and", "stormy", "night"]
+
+s2 = "apples, pears, and peaches"
+list = s2.split(", ")
+# ["apples, "pears", "and peachers"]
+
+s3 = "lions and tigers and bears"
+zoo = s3.split(/ and /)
+# ["lions", "tigers", "bears"]

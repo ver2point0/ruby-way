d3e1df71662a32c34de1023c7eb170ab56cb6ab4
John McIntosh II <jmci10@hotmail.com>
2016-06-23 12:49:08 +0000

    illustrate limit parameter

--- 2-working-with-strings/tokenize-a-string.rb
+++ 2-working-with-strings/tokenize-a-string.rb
@@ -9,3 +9,10 @@ list = s2.split(", ")
 s3 = "lions and tigers and bears"
 zoo = s3.split(/ and /)
 # ["lions", "tigers", "bears"]
+
+str = "alpha,beta,gamma,,"
+list1 = str.split(",") # ["alpha","beta","gamma"]
+list2 = str.split(",", 2) # ["alpha", "beta,gamma,,"]
+list3 = str.split(",", 4) # ["alpha", "beta", "gamma", ","]
+list4 = str.split(",", 8) # ["alpha", "beta", "gamma", "", ""]
+list5 = str.split(",", -1) # ["alpha", "beta", "gamma", "", ""]

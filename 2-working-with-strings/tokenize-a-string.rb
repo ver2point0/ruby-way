bd2490526fe088d0ec1f1554b75b81fe59b8c5c9
John McIntosh II <jmci10@hotmail.com>
2016-06-27 13:13:56 +0000

    use scan to match regex or strings against a string

--- 2-working-with-strings/tokenize-a-string.rb
+++ 2-working-with-strings/tokenize-a-string.rb
@@ -16,3 +16,7 @@ list2 = str.split(",", 2) # ["alpha", "beta,gamma,,"]
 list3 = str.split(",", 4) # ["alpha", "beta", "gamma", ","]
 list4 = str.split(",", 8) # ["alpha", "beta", "gamma", "", ""]
 list5 = str.split(",", -1) # ["alpha", "beta", "gamma", "", ""]
+
+s4 = "I am a leaf on the wind..."
+arr = s4.scan("a") # ["a","a","a"]
+arr = s4.scan(/\w+/) # ["I", "am", "a", "leaf", "on", "the", "wind"]

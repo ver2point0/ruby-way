725af919ac7970aae82b6cbd6aeb4a465c95bf53
John McIntosh II <jmci10@hotmail.com>
2016-07-01 12:45:51 +0000

    justify strings

--- 2-working-with-strings/formatting-a-string.rb
+++ 2-working-with-strings/formatting-a-string.rb
@@ -1,3 +1,8 @@
 name = "Bob"
 age = 28
 str = sprintf("Hi, %s... I see you're %d years old.", name, age)
+
+str2 = "Moby-Dick"
+s1 = str2.ljust(13) # "Moby-Dick"
+s2 = str2.center(13) # " Moby-Dick "
+s3 = str3.rjust(13) # " Moby-Dick"

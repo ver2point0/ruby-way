80b408ea6cb274f5547536776442b19ecb169a2e
John McIntosh II <jmci10@hotmail.com>
2016-07-04 15:42:55 +0000

    finish formatting strings

--- 2-working-with-strings/formatting-a-string.rb
+++ 2-working-with-strings/formatting-a-string.rb
@@ -6,3 +6,8 @@ str2 = "Moby-Dick"
 s1 = str2.ljust(13) # "Moby-Dick"
 s2 = str2.center(13) # " Moby-Dick "
 s3 = str3.rjust(13) # " Moby-Dick"
+
+str3 = "Captain Ahab"
+s4 = str3.ljust(20, "+") # "Captain Ahab++++++++"
+s5 = str3.center(20, "-") # "----Captain Ahab----"
+s6 = str4.rjust(20, "123") # 12312312Captain Ahab"

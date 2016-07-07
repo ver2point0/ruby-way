07bd1304deca60cb45838b11a24f306b9f51eab8
John McIntosh II <jmci10@hotmail.com>
2016-07-07 12:39:09 +0000

    control case

---
+++ 2-working-with-strings/control-uppercase-lowercase.rb
@@ -0,0 +1,17 @@
+# downcase: converts a string to all lowercase
+s1 = "Boston Tea Party"
+s2 = s1.downcase # "boston tea party"
+s3 = s2.upcase # "BOSTON TEA PARTY"
+
+puts s1
+puts s2
+puts s3
+
+# capitalize: capitalizes first character of a string while forcing all remaining characters to lowercase
+s4 = s1.capitalize # "Boston tea party:"
+s5 = s2.capitalize # "Boston tea party:"
+s6 = s3.capitalize # "Boston tea party:"
+
+puts s4
+puts s5
+puts s6

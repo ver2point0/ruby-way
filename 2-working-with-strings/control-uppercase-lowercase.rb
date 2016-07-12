e7f57fa639c82297638e9f30f517d488f51a9014
John McIntosh II <jmci10@hotmail.com>
2016-07-12 13:05:12 +0000

    use REGEX to detect case

--- 2-working-with-strings/control-uppercase-lowercase.rb
+++ 2-working-with-strings/control-uppercase-lowercase.rb
@@ -29,3 +29,30 @@ n2 = "abc".cascmp("XYZ") # -1
 n3 = "ABC". casecmp("xyz") # -1
 n4 = "ABC".casecmp("abc") # 0
 n5 = "xyz".casecmp("abc") # 1
+
+# in-place equivalents: upcase!, downcase!, capitalize!, swapcase!
+# no built-in methods for detecting case, but can be done with regular expressions
+
+# lowercase
+if string =~ /[a-z]/
+  puts "string contains lowercase characters"
+end
+
+# uppercase
+if string =~ /[A-Z]/
+  puts "string contains uppercase characters"
+end
+
+# mixedcase
+if string =~ /[A-Z]/ and string =~ /a-z/
+  puts "string contains mixed characters"
+end
+
+if string[0..0] =~ /[A-Z]/
+  puts "string starts with a capital letter"
+end
+
+# to match unicode uppercase or lowercase characters, use a named character class
+if string =~ /\p{Upper}/
+  puts "string contains uppercase unicode characters like Ü"
+end

ed0c200cc7b394d8b754f67f2fb2657f91b08bf2
John McIntosh II <jmci10@hotmail.com>
2016-08-03 13:08:17 +0000

    use rindex to search from the end of the string

--- 2-working-with-strings/searching-a-string.rb
+++ 2-working-with-strings/searching-a-string.rb
@@ -12,3 +12,17 @@ p pos3
 p pos4
 p pos5
 p pos6
+
+pos7 = str.rindex(?E) # 7
+pos8 = str.rindex("bert") # 2
+pos9 = str.rindex(/in/) # 13 (finds rightmost match)
+pos10 = str.rindex(?W) # nil
+pos11 = str.rindex("bart") # nil
+pos12 = str.rindex(/wein/) # nil
+
+p pos7
+p pos8
+p pos9 
+p pos10
+p pos11
+p pos12

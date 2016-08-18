776a20974a6c26f22f2a96e19500f19d251ccc5b
John McIntosh II <jmci10@hotmail.com>
2016-08-18 12:59:09 +0000

    implicit conversion to make strings & numbers equal

--- 2-working-with-strings/implicit-explicit-conversion.rb
+++ 2-working-with-strings/implicit-explicit-conversion.rb
@@ -28,3 +28,13 @@ title = title.to_str if title.respond_to?(:to_str)
 x = Helium.new
 str = "Pi #{3.14} and element #{x}"
 puts str
+
+# class Fixnum is for str2
+class Fixnum
+  def to_str
+    self.to_s
+  end
+end
+
+str2 = "The number is " + 345 # The number is 345
+puts str2

2ff3e110d9e170e01af6cfddb7ad1725e2dce43f
John McIntosh II <jmci10@hotmail.com>
2016-08-23 18:37:01 +0000

    use chomp

--- 2-working-with-strings/2.18-remove-trailing-newlines-other-characters.rb
+++ 2-working-with-strings/2.18-remove-trailing-newlines-other-characters.rb
@@ -9,3 +9,26 @@ print s2
 p s3
 print s4
 p s5
+
+str2 = "abcxyz"
+s6 = str2.chop! # "abcxy"
+
+p s6
+
+str3 = "abcxyz"
+str4 = "123\n"
+str5 = "123\r"
+str6 = "123\r\n"
+
+s7 = str3.chomp # "abcxyz"
+s8 = str4.chomp # "123"
+
+# with the default record separator, \r, \r\n, and \n are removed
+
+s9 = str5.chomp # "123"
+s10 = str6.chomp # "123"
+ 
+p s7
+p s8
+p s9
+p s10

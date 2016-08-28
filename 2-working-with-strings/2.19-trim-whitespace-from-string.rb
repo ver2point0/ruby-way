3a859da08628485874e6fb22946a2bebf6f9a68d
John McIntosh II <jmci10@hotmail.com>
2016-08-28 15:49:25 +0000

    use lstrip and rstrip

--- 2-working-with-strings/2.19-trim-whitespace-from-string.rb
+++ 2-working-with-strings/2.19-trim-whitespace-from-string.rb
@@ -9,3 +9,11 @@ p str1
 p str2
 p str3
 
+# use lstrip and strip and their in-place variants, rstrip! and lstrip! to remove whitespace from the beginning or end of a string
+str4 = " abc " 
+s1 = str4.lstrip
+s2 = str4.rstrip
+
+p str4
+p s1
+p s2

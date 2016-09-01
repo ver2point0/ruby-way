34388415a460687910abf30ea655d42e7178d02d
John McIntosh II <jmci10@hotmail.com>
2016-09-01 19:39:54 +0000

    interpolate variable values & expressions in strings

---
+++ 2-working-with-strings/2.21-embed-expressions-with-strings.rb
@@ -0,0 +1,11 @@
+temp_f = 32
+temp_c = 0
+puts "#{temp_f} Fahrenheit is #{temp_c} Celsius"
+
+a = 2
+b = 3
+c = 4
+puts "The discriminant has the value #{b * b - 4 * a * c}."
+
+word = "hello"
+puts "#{word} is #{word.reverse} spelled backward."

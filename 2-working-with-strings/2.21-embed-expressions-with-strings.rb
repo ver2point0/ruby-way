05fd68fdc50ec2eb674bb185a895e4d281ec9e38
John McIntosh II <jmci10@hotmail.com>
2016-09-03 15:47:00 +0000

    add full statements to brackets

--- 2-working-with-strings/2.21-embed-expressions-with-strings.rb
+++ 2-working-with-strings/2.21-embed-expressions-with-strings.rb
@@ -9,3 +9,12 @@ puts "The discriminant has the value #{b * b - 4 * a * c}."
 
 word = "hello"
 puts "#{word} is #{word.reverse} spelled backward."
+
+str = "The answer is #{ 
+                        def factorial(n)
+                          n == 0 ? 1 : n * factorial(n - 1)
+                        end
+                        
+                        answer = factorial(3) * 7
+                      }, of course."
+puts str

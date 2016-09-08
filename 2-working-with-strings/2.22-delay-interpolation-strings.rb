5c554f37c08d104cbe9dad9d975241116122774d
John McIntosh II <jmci10@hotmail.com>
2016-09-08 12:54:51 +0000

    add block approach to interpolate strings

--- 2-working-with-strings/2.22-delay-interpolation-strings.rb
+++ 2-working-with-strings/2.22-delay-interpolation-strings.rb
@@ -5,3 +5,11 @@ str = '#{name} is my name, and #{nation} is my nation.'
 name, nation = "Jane Doe", "Spain"
 s1 = eval('"' + str + '"') # Jane Doe is my name, and Spain is my nation.
 p s1
+
+#2. block approach
+str1 = proc do |name, nation|
+  "#{name} is my name, and #{nation} is my nation."
+end
+
+s2 = str1.call("Gulliver Foyle", "Terra") # Gulliver Foyle is my name, and Terra is my nation.
+p s2
